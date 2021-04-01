350389f8ef9a1be14379d2f3680bb7eec9d40a97
NoaAzuz <noaazuz@gmail.com>
2021-04-01 16:06:39 +0300

    first

---
+++ text.py
@@ -0,0 +1,26 @@
+def revword(word):
+    note=''
+    length= len(word)
+    j=length-1
+    while j >= 0:
+        note=note+word[j].lower()
+        j=j-1
+    return note
+  
+def countword():
+    file= open("C:/Users/Noa Azuz/Desktop/Studies/KnowledgeAndData/TwoTask/text.txt","r")
+    countWord=0
+    saveWord=file.readline()
+    saveWord=saveWord.strip()
+    txt=file.readlines()
+    for line in txt:
+        for note in line.split():
+            note= revword(note)
+            if note==saveWord:
+                countWord= countWord+1
+    return countWord+1
+    #print(saveWord)
+
+
+
+
